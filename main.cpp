a2796c72ea73d22b4507106490058730909de8cc
Justyna Walkowiak <justyna.walkowiak@nokia.com>
2021-06-03 19:29:21 +0200

    refactor

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <array>
 #include <cmath>
 #include <random>
+#include <unordered_map>
 using namespace std;
 
 class parameters
@@ -22,14 +23,15 @@ public:
     double IB; // proportion of inflexibles among supporters of B
     double pB; // proportion of supporters of B
     bool selfInfluenceMatters = 1;
-    double initsupEnd;
-    double initsupStep;
-    double inflexEnd;
-    double inflexStep;
+    double pA_End;
+    double pA_Step;
+    double IA_End;
+    double IA_Step;
     // int numOfKwadraciki;
-    double initial_pA;
-    double initial_IA;
-    double initial_pB;
+    double pA_Begin;
+    double IA_Begin;
+    double pB_Begin;
+    double conv;
 
     parameters()
     {
@@ -47,19 +49,20 @@ public:
              << "IB = "<< IB << endl
              << "pB = "<< pB << endl
              << "selfInfluenceMatters = " << selfInfluenceMatters << endl
-             << "initsupEnd = " << initsupEnd << endl
-             << "initsupStep = " << initsupStep << endl
-             << "inflexEnd = " << inflexEnd << endl
-             << "inflexStep = " << inflexStep << endl << endl;
+             << "pA_End = " << pA_End << endl
+             << "pA_Step = " << pA_Step << endl
+             << "IA_End = " << IA_End << endl
+             << "IA_Step = " << IA_Step << endl
+             << "conv = " << conv << endl << endl;
     }
 
     void parseInput()
     {
         ifstream file;
         file.open("params_file.txt", ifstream::in);
-        string M_, n_, g_, s_, IA_, pA_, IB_, selfInfluenceMatters_, initsupEnd_, initsupStep_, inflexEnd_, inflexStep_;
-        file >> M_ >> n_ >> g_ >> s_ >> IA_ >> pA_ >> IB_ >> selfInfluenceMatters_ >> initsupEnd_ >> initsupStep_ >>
-            inflexEnd_ >> inflexStep_;
+        string M_, n_, g_, s_, IA_, pA_, IB_, selfInfluenceMatters_, pA_End_, pA_Step_, IA_End_, IA_Step_, conv_;
+        file >> M_ >> n_ >> g_ >> s_ >> IA_ >> pA_ >> IB_ >> selfInfluenceMatters_ >> pA_End_ >> pA_Step_ >>
+            IA_End_ >> IA_Step_, conv_;
         M = stoi(M_);
         n = stoi(n_);
         g = stoi(g_);
@@ -68,14 +71,15 @@ public:
         pA = stod(pA_);
         IB = stod(IB_);
         selfInfluenceMatters = (stoi(selfInfluenceMatters_) == 0) ? false : true;
-        initsupEnd = stod(initsupEnd_);
-        initsupStep = stod(initsupStep_);
-        inflexEnd = stod(inflexEnd_);
-        inflexStep = stod(inflexStep_);
+        pA_End = stod(pA_End_);
+        pA_Step = stod(pA_Step_);
+        IA_End = stod(IA_End_);
+        IA_Step = stod(IA_Step_);
         pB = 1 - pA;
-        initial_pA = pA;
-        initial_IA = IA;
-        initial_pB = pB;
+        pA_Begin = pA;
+        IA_Begin = IA;
+        pB_Begin = pB;
+        conv = stod(conv_);
     }
 };
 
@@ -85,6 +89,7 @@ class randBin
     random_device rd;
     mt19937 gen;
     uniform_int_distribution<int> dis;
+    uniform_real_distribution<double> dis_real;
     int num;
 public:
     randBin(const int _max_nr)
@@ -92,12 +97,15 @@ public:
     {
         gen = mt19937(rd());
         dis = uniform_int_distribution<int>(0, max_nr-1); //Parameters->n-1
-        // std::srand(std::time(nullptr));
+        dis_real = uniform_real_distribution<double>(0.0, 1.0);
     }
     int genRandBinNumber()
     {
-        // return rand()%max_nr;
         return dis(gen);
+    }
+    double getRandUniform()
+    {
+        return dis_real(gen);
     }
     randBin(const randBin&) = delete;
 };
@@ -110,7 +118,7 @@ string toStringWithPrecision(const double value, int prec)
     return out.str();
 }
 
-void writeToFileHistogram(parameters* params, double val)
+ofstream createAndOpenFileHistogram(parameters* params)
 {
     ofstream file;
     string name = "HIS__M" + to_string(params->M) + "_n" + to_string(params->n) + "_g" + to_string(params->g) + "_s" +
@@ -118,55 +126,30 @@ void writeToFileHistogram(parameters* params, double val)
         "_IB" + toStringWithPrecision(params->IB, 1) + "_pB" + toStringWithPrecision(params->pB, 1) +
         "_selfInfl" + (params->selfInfluenceMatters ? "true" : "false") + ".txt";
 
-    file.open(name, ios::out | ios::app);
-    file << val << "\n";
-
-    file.close();
-}
-
-void cleanHistogramFile(parameters params)
-{
-    ofstream file;
-    string name = "HIS__M" + to_string(params.M) + "_n" + to_string(params.n) + "_g" + to_string(params.g) + "_s" +
-        to_string(params.s) + "_IA" + toStringWithPrecision(params.IA, 1) + "_pA" + toStringWithPrecision(params.pA, 1) +
-        "_IB" + toStringWithPrecision(params.IB, 1) + "_pB" + toStringWithPrecision(params.pB, 1) +
-        "_selfInfl" + (params.selfInfluenceMatters ? "true" : "false") + ".txt";
-
     file.open(name, ios::out);
     file.close();
+    file.open(name, ios::out | ios::app);
+
+    return file;
 }
 
 ofstream createAndOpenFileKwadraty(parameters* params)
 {
     ofstream file;
     string name = "KWA__M" + to_string(params->M) + "_n" + to_string(params->n) + "_g" + to_string(params->g) + "_s" +
-        to_string(params->s) + "_IA" + toStringWithPrecision(params->initial_IA, 1) + "_pA" +
-        toStringWithPrecision(params->initial_pA, 1) + "_IB" + toStringWithPrecision(params->IB, 1) + "_pB" +
-        toStringWithPrecision(params->initial_pB, 1) + "_selfInfl" + (params->selfInfluenceMatters ? "true" : "false") +
-        "_initsupEnd" + toStringWithPrecision(params->initsupEnd, 2) +
-        "_initsupStep" + toStringWithPrecision(params->initsupStep, 2) +
-        "_inflexEnd" + toStringWithPrecision(params->inflexEnd, 2) +
-        "_inflexStep" + toStringWithPrecision(params->inflexStep, 2) + ".txt";
-
-    file.open(name, ios::out | ios::app);
-
-    return file;
-}
-
-void cleanKwadratyFile(parameters params)
-{
-    ofstream file;
-    string name = "KWA__M" + to_string(params.M) + "_n" + to_string(params.n) + "_g" + to_string(params.g) + "_s" +
-        to_string(params.s) + "_IA" + toStringWithPrecision(params.initial_IA, 1) + "_pA" +
-        toStringWithPrecision(params.initial_pA, 1) + "_IB" + toStringWithPrecision(params.IB, 1) + "_pB" +
-        toStringWithPrecision(params.initial_pB, 1) +  "_selfInfl" + (params.selfInfluenceMatters ? "true" : "false") +
-        "_initsupEnd" + toStringWithPrecision(params.initsupEnd, 2) +
-        "_initsupStep" + toStringWithPrecision(params.initsupStep, 2) +
-        "_inflexEnd" + toStringWithPrecision(params.inflexEnd, 2) +
-        "_inflexStep" + toStringWithPrecision(params.inflexStep, 2) + ".txt";
+        to_string(params->s) + "_IA" + toStringWithPrecision(params->IA_Begin, 1) + "_pA" +
+        toStringWithPrecision(params->pA_Begin, 1) + "_IB" + toStringWithPrecision(params->IB, 1) + "_pB" +
+        toStringWithPrecision(params->pB_Begin, 1) + "_selfInfl" + (params->selfInfluenceMatters ? "true" : "false") +
+        "_pA_End" + toStringWithPrecision(params->pA_End, 2) +
+        "_pA_Step" + toStringWithPrecision(params->pA_Step, 2) +
+        "_IA_End" + toStringWithPrecision(params->IA_End, 2) +
+        "_IA_Step" + toStringWithPrecision(params->IA_Step, 2) + ".txt";
 
     file.open(name, ios::out);
     file.close();
+    file.open(name, ios::out | ios::app);
+
+    return file;
 }
 
 class agents
@@ -185,10 +168,6 @@ public:
         int nOfRacionalA = std::round(Parameters->n * Parameters->pA - nOfInflexiblesA);
         int nOfInflexiblesB = std::round(Parameters->n * Parameters->IB * Parameters->pB);
         int nOfRacionalB = std::round(Parameters->n * Parameters->pB - nOfInflexiblesB);
-        // cout << "IA: " << Parameters->IA << endl;
-        // cout << "he?: " << (int)(Parameters->n * Parameters->pB - nOfInflexiblesB + 0.5) << endl;
-        // cout << "pA: " << Parameters->pA << "  pB: " << Parameters->pB << endl;  
-        // cout << nOfInflexiblesA << endl << nOfRacionalA << endl  << nOfInflexiblesB  << endl << nOfRacionalB << endl ;
 
         for (int i = 0; i < nOfInflexiblesA; i++)
             tableOfAgents.push_back(50.5);
@@ -199,7 +178,12 @@ public:
         for (int i = 0; i < nOfRacionalB; i++)
             tableOfAgents.push_back(-0.5);
 
-        //cout<< "Vector size = " << tableOfAgents.size() << endl;
+        // if (Parameters->conv > 0)
+        //     for (auto agent: tableOfAgents) {
+        //         if (RandBin->getRandUniform() < Parameters->conv)
+        //             agent.second = 2;
+        //         else agent.second = 1;
+        //     }
     }
 
     void interactions()
@@ -208,38 +192,38 @@ public:
         for(int i = 0; i<Parameters->s; i++)
         {
             agentsIdxs.push_back(RandBin->genRandBinNumber());
-            //cout<<agentsIdxs.back()<<endl;
         }
         int nrOfInteractions = 0;
         while(1)
         {
             if(Parameters->selfInfluenceMatters == true)
             {
-                for(auto idExt : agentsIdxs)
-                    for(auto idInt : agentsIdxs)
-                        tableOfAgents[idExt] += (tableOfAgents[idInt] > 0) ? 1 : -1;
+                int sum = 0;
+                for(auto idx : agentsIdxs)
+                    sum += (tableOfAgents[idx] > 0) ? 1 : -1;
+                for(auto idx : agentsIdxs)
+                    tableOfAgents[idx] += sum;
             }
             else if (Parameters->selfInfluenceMatters == false)
             {
-                for(auto idExt : agentsIdxs)
-                    for(auto idInt : agentsIdxs)
-                    {
-                        if (idExt == idInt) continue;
-                        tableOfAgents[idExt] += (tableOfAgents[idInt] > 0) ? 1 : -1;
+                unordered_map<int, double> hm;
+                for(auto idExt : agentsIdxs) {
+                    int suma = 0;
+                    for(auto idIn : agentsIdxs) {
+                        if(idExt != idIn)
+                            suma += (tableOfAgents[idIn] > 0) ? 1 : -1;
                     }
+                    hm[idExt] = suma;
+                }
+                for(auto idExt : agentsIdxs)
+                    tableOfAgents[idExt] += hm[idExt];
             }
             nrOfInteractions++;
-            vector<double> tableOfAgentsSmall;
-            tableOfAgentsSmall.reserve(Parameters->s);
-            for (auto idx : agentsIdxs)
-            {
-                tableOfAgentsSmall.push_back(tableOfAgents[idx]);
-            }
             if(nrOfInteractions >= Parameters->M)
                 break;
-            else if(all_of(tableOfAgentsSmall.begin(), tableOfAgentsSmall.end(), [](double v){return (v < 0);}))
+            else if(all_of(agentsIdxs.begin(), agentsIdxs.end(), [this](double v){return (tableOfAgents[v] < 0);}))
                 break;
-            else if(all_of(tableOfAgentsSmall.begin(), tableOfAgentsSmall.end(), [](double v){ return (v > 0);}))
+            else if(all_of(agentsIdxs.begin(), agentsIdxs.end(), [this](double v){ return (tableOfAgents[v] > 0);}))
                 break;
         }
     }
@@ -253,8 +237,10 @@ public:
 
     void saveToFile()
     {
+        auto file = createAndOpenFileHistogram(Parameters);
         for (auto v : tableOfAgents)
-            writeToFileHistogram(Parameters, v);
+            file << v << "\n";
+        file.close();
     }
     double countProportionOfBToAll()
     {
@@ -263,14 +249,9 @@ public:
         int k = 0;
         for(auto agent : tableOfAgents)
         {
-            // cout << k++ << ")" << agent << " ";
             countB += (agent < 0) ? 1 : 0;
         }
-        // cout << endl;
         proportion = (double) countB / Parameters->n;
-        // if (proportion > 1.0)
-        // cout << "PARAMS: pA= " << Parameters->pA << " IA=" << Parameters->IA << endl;
-        std::cout << "proportion: " << proportion << "   countB: " << countB << "   n: " << Parameters->n << std::endl;
         return proportion;
     }
 };
@@ -280,26 +261,27 @@ class kwadrats
 public:
     randBin *RandBin;
     parameters* Parameters;
-    int initialSupportersDim = std::round((Parameters->initsupEnd - Parameters->initial_pA)/Parameters->initsupStep + 1);
-    int inflexiblesDim = std::round((Parameters->inflexEnd - Parameters->initial_IA)/Parameters->inflexStep + 1);
+    int pB_Dim = std::round((Parameters->pA_End - Parameters->pA_Begin)/Parameters->pA_Step + 1);
+    int IA_Dim = std::round((Parameters->IA_End - Parameters->IA_Begin)/Parameters->IA_Step + 1);
     vector<vector<double>> table;
     void liczKwadraciki()
     {
         double progress = 0;
         int iterationNum = 0;
-        const int numOfIterations = initialSupportersDim * inflexiblesDim;
-        table.resize(inflexiblesDim, vector<double>(initialSupportersDim));
-        Parameters->IA = Parameters->initial_IA;
+        const int numOfIterations = pB_Dim * IA_Dim;
+        table.resize(IA_Dim, vector<double>(pB_Dim));
+        Parameters->IA = Parameters->IA_Begin;
+        int numOfRealizations = 20;
         double sumOfProportion = 0;
 
-        for(int i = 0; i < inflexiblesDim; i++)
+        for(int i = 0; i < IA_Dim; i++)
         {
-            Parameters->pA = Parameters->initial_pA;
-            for(int j = 0; j < initialSupportersDim; j++)
+            Parameters->pA = Parameters->pA_Begin;
+            for(int j = 0; j < pB_Dim; j++)
             {
-                for (int realization = 0; realization < 20; realization++)
+                sumOfProportion = 0;
+                for (int realization = 0; realization < numOfRealizations; realization++)
                 {
-                    cout << "realization nr " << realization+1 << endl;
                     agents Agents(Parameters, RandBin);
                     for(int k = 0; k < Parameters->g; k++)
                     {
@@ -308,16 +290,15 @@ public:
                     table[i][j] = Agents.countProportionOfBToAll();
                     sumOfProportion += table[i][j];
                 }
-                Parameters->pA += Parameters->initsupStep;
+                Parameters->pA += Parameters->pA_Step;
                 progress = (double) ++iterationNum/numOfIterations;
 
                 if (iterationNum % 10 == 0)
                     cout << "Progress: " << toStringWithPrecision(100*progress,2) << "%" << endl;
 
-                double average = sumOfProportion / 20;
-                cout << "AVG = " << average << endl;
+                table[i][j] = (double)sumOfProportion / numOfRealizations;
             }
-            Parameters->IA += Parameters->inflexStep;
+            Parameters->IA += Parameters->IA_Step;
         }
     }
     kwadrats(parameters* _Parameters, randBin * _RandBin)
@@ -325,17 +306,17 @@ public:
     {}
     void display()
     {
-        int initialSupportersDim = std::round((Parameters->initsupEnd - Parameters->initial_pA)/Parameters->initsupStep + 1);
-        int inflexiblesDim = std::round((Parameters->inflexEnd - Parameters->initial_IA)/Parameters->inflexStep + 1);
+        int pB_Dim = std::round((Parameters->pA_End - Parameters->pA_Begin)/Parameters->pA_Step + 1);
+        int IA_Dim = std::round((Parameters->IA_End - Parameters->IA_Begin)/Parameters->IA_Step + 1);
         auto file = createAndOpenFileKwadraty(Parameters);
-        for(int i = (inflexiblesDim-1); i >= 0; i--)
+        for(int i = (IA_Dim-1); i >= 0; i--)
         {
-            for(int j = 0; j < initialSupportersDim; j++)
+            for(int j = 0; j < pB_Dim; j++)
             {
-                // cout<< table[i][j] << "\t";
+                cout << toStringWithPrecision(table[i][j], 3) << "\t";
                 file << toStringWithPrecision(table[i][j], 3) << "\t";
             }
-            // cout<<endl;
+            cout<<endl;
             file << "\n";
         }
         file.close();
@@ -354,7 +335,6 @@ int main(int argc, char ** argv)
 
         if (simulationType == "histogram")
         {
-            cleanHistogramFile(Parameters);
             agents Agents(&Parameters, &RandBin);
             for(int i = 0; i < Parameters.g; i++)
                 Agents.interactions();
@@ -363,7 +343,6 @@ int main(int argc, char ** argv)
         }
         else if (simulationType == "kwadraty")
         {
-            cleanKwadratyFile(Parameters);
             kwadrats Kwadrats(&Parameters, &RandBin);
             Kwadrats.liczKwadraciki();
             Kwadrats.display();
